60c0b40f2fbcc6b0b64d4f3ad7b885b357b1b1c2
aishwarya <ashp17star@gmail.com>
2022-12-05 14:58:26 +0530

    ConstraintLayoutDemo

---
+++ ConstraintLayoutDemo/app/src/main/java/com/example/constraintlayoutdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.constraintlayoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSubmit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Form is Submitted!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
